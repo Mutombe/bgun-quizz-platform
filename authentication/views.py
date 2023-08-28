aa5ca6fd9f1060826cfdc6fdfc725f6d2d6cf4ce
Mutombe <simbarashemutombe1@gmail.com>
2023-08-28 21:37:18 +0300

    registration

--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import logout
 
@@ -8,8 +9,12 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
+            user = form.cleaned_data.get("username")
+            messages.success(request, "Account was created for " + user)
             return redirect('login')
+            
     else:
+        
         form = UserCreationForm()
     return render(request, 'registration/register.html', {'form': form})
 
